7bb694e8783f8e41fbe815c4f33db01222c2c541
Luca Barone <baro.luc@gmail.com>
2017-03-29 16:13:51 +0200

    WFE-405 re-enable play and win events

--- src/helpers/segment.jsx
+++ src/helpers/segment.jsx
@@ -1,31 +1,51 @@
-import emitter from "../emitter";
-import {canUseDOM} from 'fbjs/lib/ExecutionEnvironment';
+import emitter from '../emitter'
+import {canUseDOM} from 'fbjs/lib/ExecutionEnvironment'
 
-let identifySubscription;
+let playSubscription, winSubscription
 
 export default {
-  enable(){
-    if(canUseDOM) {
-      if(typeof analytics === "undefined") {
-        const error = new Error("React A/B Test Segment Helper: 'analytics' global is not defined.");
-        error.type = "PUSHTELL_HELPER_MISSING_GLOBAL";
-        throw error;
+  enable() {
+    if (canUseDOM) {
+      if (typeof analytics === 'undefined') {
+        const error = new Error('React A/B Test Segment Helper: "analytics" global is not defined.')
+        error.type = 'PUSHTELL_HELPER_MISSING_GLOBAL'
+        throw error
       }
-      identifySubscription = emitter.addPlayListener((experimentName, variantName) => {
-        analytics.identify({[experimentName]: variantName}, () =>
-          emitter.emit('segment-play', experimentName, variantName)
-        )
-      })
+      
+      playSubscription = emitter.addPlayListener(
+        (experimentName, variationName) => {
+          analytics.identify(
+            {[experimentName]: variationName}, 
+            () => emitter.emit('segment-play', experimentName, variationName),
+          )
+          
+          analytics.track(
+            'Experiment Viewed', 
+            {experimentName, variationName}, 
+            () => emitter.emit('segment-play', experimentName, variationName),
+          )
+        }
+      )
+      
+      winSubscription = emitter.addWinListener(
+        (experimentName, variationName) =>
+          analytics.track(
+            'Experiment Won', 
+            {experimentName, variationName}, 
+            () => emitter.emit('segment-win', experimentName, variationName)
+          )
+      )
     }
   },
-  disable(){
-    if(canUseDOM) {
-      if(!playSubscription || !winSubscription) {
-        const error = new Error("React A/B Test Segment Helper: Helper was not enabled.");
-        error.type = "PUSHTELL_HELPER_INVALID_DISABLE";
-        throw error;
+  disable() {
+    if (canUseDOM) {
+      if (!playSubscription || !winSubscription) {
+        const error = new Error('React A/B Test Segment Helper: Helper was not enabled.')
+        error.type = 'PUSHTELL_HELPER_INVALID_DISABLE'
+        throw error
       }
-      identifySubscription.remove()
+      playSubscription.remove()
+      winSubscription.remove()
     }
   }
 }
